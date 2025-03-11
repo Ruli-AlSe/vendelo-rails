8eca1c4b025397f49e7bc9d22e81be7d1696acd5
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-03-11 01:43:04 -0600

    Add optimizations according with DRY principle and add internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get "/products", to: "products#index"
-  get "/products/new", to: "products#new", as: :new_product
-  post "/products", to: "products#create"
-  patch "/products/:id", to: "products#update"
-  delete "/products/:id", to: "products#destroy"
-  get "/products/:id", to: "products#show", as: :product
-  get "/products/:id/edit", to: "products#edit", as: :edit_product
+  # get "/products", to: "products#index"
+  # get "/products/new", to: "products#new", as: :new_product
+  # post "/products", to: "products#create"
+  # patch "/products/:id", to: "products#update"
+  # delete "/products/:id", to: "products#destroy"
+  # get "/products/:id", to: "products#show", as: :product
+  # get "/products/:id/edit", to: "products#edit", as: :edit_product
+  resources :products, path: "/"
 end
