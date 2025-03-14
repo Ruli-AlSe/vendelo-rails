e3a48b3125f8e90daaf1385643f4ade265f7219d
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-03-13 20:49:27 -0600

    Create session when user logs in or create an account and protect pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,8 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    def login
+      post sessions_path, params: { login: "paco@vendelo.com", password: "123456" }
+    end
   end
 end
