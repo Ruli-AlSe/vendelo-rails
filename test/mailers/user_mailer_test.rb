69644591a507f975eb04bb987cd8ef6567c47a0d
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-03-18 03:58:34 -0600

    Create user_mailer to send emails to each new user in the platform and configure letter_opener gem to see emails in the browser in development env

---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class UserMailerTest < ActionMailer::TestCase
+  def setup
+    @user = User.last
+  end
+  test "welcome" do
+    mail = UserMailer.with(user: @user).welcome
+    assert_equal "¡Bienvenido a Vendelo!", mail.subject
+    assert_equal [ @user.email ], mail.to
+    assert_equal [ "no-reply@vendelo.com" ], mail.from
+    assert_match "Hey #{@user.username}", mail.body.encoded
+  end
+end
