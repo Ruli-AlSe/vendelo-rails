d5b3fc5a5aae1433f83896734d4ef8c2860bf389
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-03-18 21:04:21 -0600

    Use actioncable to reload product pages when there are users visiting them and the admin updates the product

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "tailwindcss-stimulus-components" # @6.1.3
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.0.100
